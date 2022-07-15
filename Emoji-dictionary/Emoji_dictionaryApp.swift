9fbc1d57458c59d25ac7d23cdbdca1cec1e7040a
Vladislavs Bužinskis <intelifoxgames@gmail.com>
2022-07-15 16:12:00 +0300

    Initial Commit

---
+++ Emoji-dictionary/Emoji_dictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_dictionaryApp.swift
+//  Emoji-dictionary
+//
+//  Created by Vladislavs Buzinskis on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_dictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
